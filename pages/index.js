e521a62cb12155f58b52940e5175c7bd05cb3207
meghanapula <meghanapula3@gmail.com>
2025-06-06 12:41:38 -0500

    Updated homepage text

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-indigo-500 to-pink-500 text-white text-2xl font-bold">
-      Tailwind is working! 🎉
+      hi! 🎉
     </div>
   );
 }
